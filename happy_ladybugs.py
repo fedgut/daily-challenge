77d2049418f5d83ab55240666ddfc54ab2e6ccf3
fedgut <fedgut@gmail.com>
2019-09-10 10:02:10 -0500

    adding a ladybug solution seen in coments

--- happy_ladybugs.py
+++ happy_ladybugs.py
@@ -18,3 +18,15 @@ def happyLadybugs(b):
             return "NO"
     
     return "YES"
+
+
+#Beautifull solution seen in forum
+    for a in set(b):
+        if a != "_" and b.count(a) == 1:
+            return "NO"
+    
+    if b.count("_") == 0:
+        for i in range(1,n-1):
+            if b[i-1]!=b[i] and b[i+1]!=b[i]:
+                return "NO"
+    return "YES"
