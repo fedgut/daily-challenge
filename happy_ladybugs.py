caf4992adedf4fe96793059edd80ebeb4f95c77f
fedgut <fedgut@gmail.com>
2019-09-10 09:55:24 -0500

    adding ladybug commnets

--- happy_ladybugs.py
+++ happy_ladybugs.py
@@ -1,19 +1,17 @@
 def happyLadybugs(b):
 
     import collections
+
     
-    if b == "_":
-        return "YES"
-    
-    counter = collections.Counter(b)
+    counter = collections.Counter(b)    #if there is a ladybug that doesnt have a color partner, return no
     for key, value in counter.items():
         if key != "_" and value == 1:
             return "NO"
     
-    if "_" in b:
+    if "_" in b:                     # else, if there is space to sort return yes
         return "YES"
 
-    for indx in range (1, len(b)):
+    for indx in range (1, len(b)):    #else, if there is no space but bugs are already sorted return yes (lines 14- 20)
         if b[indx] == b[indx-1] or b[indx] == b[indx+1]:
             continue
         else:
