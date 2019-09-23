568fad4fb2c15b6d41515d86579cdcef1d409f72
fedgut <fedgut@gmail.com>
2019-09-23 09:53:53 -0500

    Add Divisible sum pairs solution

---
+++ divisible_sum_pairs.py
@@ -0,0 +1,10 @@
+def divisibleSumPairs(n, k, ar):
+
+    ar.sort()
+    count = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            if (ar[i]+ar[j]) % k == 0:
+                count += 1
+
+    return (count)
--- equalize_the_array.py
+++ equalize_the_array.py
@@ -1,5 +1,7 @@
+def ecualize(arr):
+
     unique = set(arr)
     times_n_appears=[] 
-    
+
     for each_n in unique:
         times_n_appears.append(arr.count(each_n))
