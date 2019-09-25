f6e79fbe063d526d722efdb0260bc3da01f1a5d7
fedgut <fedgut@gmail.com>
2019-09-25 11:40:25 -0500

    Add insertion_sort2 solution

--- insertion_sort2.py
+++ insertion_sort2.py
@@ -4,10 +4,14 @@ arr = [1, 4, 3, 5, 6, 2]
 
 def insertionSort2(n, arr):
 
-    is_sorted = false
-    arr_w = [srt(i) for i in arr]
+    for i in range(1, len(arr)):
+        item = arr[i]
+        j = i
+        while j > 0 and item < arr[j-1]:
+            arr[j] = arr[j-1]
+            j -= 1
+        arr[j] = item
+        print(" ".join(str(j) for j in arr))
 
-    while is_sorted == False:
-        for indx in range(1, len(arr_w), 1):
-            if arr_w[indx] > arr_w[indx + 1] and arr_w[indx] > arr_w[indx - 1]:
-                insert.arr_w
+
+print(insertionSort2(n, arr))
